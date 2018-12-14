5ded6d8b5f6446f46c7761acb6c03f988185f08e
Alex Percsi <alex.percsi@humanitec.com>
2018-12-14 16:30:19 +0100

    Fix TS error in environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  API_URL: process.env.API_BASE_URL,
-  DOCS_API: process.env.DOCS_API,
-  OAUTH_CLIENT_ID: process.env.OAUTH_CLIENT_ID,
-  OAUTH_TOKEN_URL:  process.env.OAUTH_TOKEN_URL,
+  API_URL: (process as any).env.API_BASE_URL,
+  DOCS_API: (process as any).env.DOCS_API,
+  OAUTH_CLIENT_ID: (process as any).env.OAUTH_CLIENT_ID,
+  OAUTH_TOKEN_URL:  (process as any).env.OAUTH_TOKEN_URL,
   production: true
 };
