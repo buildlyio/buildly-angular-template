1a73064d1367d6e50363856ebf3cb61e2a94d9f1
Alex Percsi <alex.percsi@humanitec.com>
2018-12-14 16:36:12 +0100

    Environment variables hack

---
+++ src/environments/environment.prod.js
@@ -0,0 +1,7 @@
+export const environment = {
+  API_URL: process.env.API_BASE_URL,
+  DOCS_API: process.env.DOCS_API,
+  OAUTH_CLIENT_ID: process.env.OAUTH_CLIENT_ID,
+  OAUTH_TOKEN_URL:  process.env.OAUTH_TOKEN_URL,
+  production: true
+};
