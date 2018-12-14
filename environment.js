ebf30baf645342f9ddef630221c0d6b31e241aae
Alex Percsi <alex.percsi@humanitec.com>
2018-12-14 17:06:08 +0100

    Fix env variables

--- environment.js
+++ environment.js
@@ -1,19 +1,9 @@
 module.exports = {
   environment: {
-      API_BASE_URL: process.env.API_BASE_URL,
-      BASE_URL: process.env.BASE_URL,
-      ELASTIC_SEARCH_URL: process.env.ELASTIC_SEARCH_URL,
-      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-      GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+      API_URL: process.env.API_URL,
+      DOCS_API: process.env.DOCS_API,
       OAUTH_CLIENT_ID: process.env.OAUTH_CLIENT_ID,
-      OAUTH_CLIENT_SECRET: process.env.OAUTH_CLIENT_SECRET,
-      OAUTH_LOGIN_URL: process.env.OAUTH_LOGIN_URL,
-      OAUTH_REDIRECT_URI: process.env.OAUTH_REDIRECT_URI,
-      OAUTH_SCOPE: process.env.OAUTH_SCOPE,
-      OAUTH_TOKEN_ERROR_CODE: process.env.OAUTH_TOKEN_ERROR_CODE,
       OAUTH_TOKEN_URL: process.env.OAUTH_TOKEN_URL,
-      OAUTH_USER_URL: process.env.OAUTH_USER_URL,
-      TRACK_URL: process.env.TRACK_URL,
       production: process.env.production
   }
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// `ng build --prod --app=toladata` will build with these variables
 
-export const environment = {
-    API_URL: 'http://35.190.36.72/api',
-    DOCS_API: 'http://35.241.189.47/api',
-    OAUTH_CLIENT_ID: 'lBDR6OTSJHi7VwmtMPOObB8GRs9AeEarhXyFciOh',
-    OAUTH_TOKEN_URL:  'http://35.190.36.72/oauth/token/',
-    production: false
-  };
-  
-  /*
-   * In development mode, for easier debugging, you can ignore zone related error
-   * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
-   * below file. Don't forget to comment it out in production mode
-   * because it will have a performance impact when errors are thrown
-   */
-  // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
-  
+export const environment = (window as any).environment;
+
+
