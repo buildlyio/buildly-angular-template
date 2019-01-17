1ef5231e82b118bfc74587575d351dd12702efd0
Aziz Haddad <aziz-haddad@users.noreply.github.com>
2019-01-17 12:11:56 +0100

    add app entry point to env variables

--- environment.js
+++ environment.js
@@ -4,6 +4,7 @@ module.exports = {
       DOCS_API: process.env.DOCS_API,
       OAUTH_CLIENT_ID: process.env.OAUTH_CLIENT_ID,
       OAUTH_TOKEN_URL: process.env.OAUTH_TOKEN_URL,
+      APP_ENTRY_POINT: process.env.APP_ENTRY_POINT,
       production: process.env.production
   }
 };
