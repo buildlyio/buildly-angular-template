2c28a3a9f7c4099f7bf80e7806ac318dc1648239
Aziz Haddad <aziz-haddad@users.noreply.github.com>
2019-01-30 16:34:10 +0100

    configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,15 +2,21 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   API_URL: 'http://35.190.36.72',
+//   OAUTH_CLIENT_ID: 'lBDR6OTSJHi7VwmtMPOObB8GRs9AeEarhXyFciOh',
+//   OAUTH_TOKEN_URL:  'http://35.190.36.72/oauth/token/',
+//   APP_ENTRY_POINT: '/products',
+//   production: false
+// };
+
 export const environment = {
-  API_URL: 'http://35.190.36.72',
-  DOCS_API: 'http://35.190.36.72',
-  OAUTH_CLIENT_ID: 'lBDR6OTSJHi7VwmtMPOObB8GRs9AeEarhXyFciOh',
-  OAUTH_TOKEN_URL:  'http://35.190.36.72/oauth/token/',
+  API_URL: 'https://kupfer-dev-api.walhall.io/',
+  OAUTH_CLIENT_ID: 'x3aqs246YzriNk5I_DN_Ff4ojPxIUesHK7ZyG50LB3Y',
+  OAUTH_TOKEN_URL:  'https://kupfer-dev-api.walhall.io/oauth/token/',
   APP_ENTRY_POINT: '/products',
   production: false
 };
-
 /*
  * In development mode, for easier debugging, you can ignore zone related error
  * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
