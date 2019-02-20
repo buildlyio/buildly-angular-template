b7f421061f774f37ebc7cd7cf81fd6588a814462
Aziz <aziz@humanitec.com>
2019-02-20 10:37:36 +0100

    add freyja theming and make app.components empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { OAuthService } from '@libs/midgard-angular/src/lib/modules/oauth/oauth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(
-    private oauthService: OAuthService,
-    private router: Router
-  ) {}
-
-  ngOnInit() {
-    if (!this.oauthService.hasValidAccessToken()) {
-      this.router.navigate(['/login']);
-    }
-  }
-}
+export class AppComponent {}
