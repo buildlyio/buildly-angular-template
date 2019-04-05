ddf1fa09a4929785bd2ead04b26f5cecd87e7938
Aziz <aziz@humanitec.com>
2019-04-05 10:24:01 +0200

    modules has been added to the application by midgard-schematics

--- src/midgard/modules/store/store.module.ts
+++ src/midgard/modules/store/store.module.ts
@@ -10,8 +10,9 @@ import { DashboardsEpics } from '@clients/dashboards/src/lib/state/dashboards.ep
 import { ProductsEpics } from '@clients/products/src/lib/state/products.epics';
 import { DocumentsEpics } from '@clients/documents/src/lib/state/documents.epics';
 import { ClientEpics } from '@clients/blueprint-client/src/lib/state/blueprint-client.epics';
+import { LocationsEpics } from '@clients/locations/src/lib/state/locations.epics';
 
-@NgModule({  providers: [DashboardsEpics, ProductsEpics, DocumentsEpics, ClientEpics]
+@NgModule({  providers: [DashboardsEpics, ProductsEpics, DocumentsEpics, ClientEpics, LocationsEpics]
 })
 export class MidgardStoreModule {
   static forRoot(): ModuleWithProviders {
