91ea40016e117f6618859f37e48c59d3a89aa6fa
Aziz <aziz@humanitec.com>
2019-04-11 14:54:22 +0200

    fix env script

--- initialize_container.sh
+++ initialize_container.sh
@@ -6,8 +6,10 @@ export GIT_FETCH_HEAD=`cat .git/FETCH_HEAD`
 #Read all environment variables as output by printenv and put them into an object stored in window.environment
 RESULT='window.environment = {'
 while read line; do
-  #Note that multi-line environment variable values will break this
-  RESULT+='"'`echo $line | sed --expression='s/=/\":\"/g'`'"',
+  if [ $line = "API_URL" ] | [ $line = "OAUTH_CLIENT_ID" ] | [ $line = "OAUTH_TOKEN_URL" ] ; then
+    #Note that multi-line environment variable values will break this
+    RESULT+='"'`echo $line | sed --expression='s/=/\":\"/g'`'"',
+  fi
 done < <(printenv)
 #Add production flag which is typically found in environment.prod.ts
 RESULT+='"production":"true"}'
