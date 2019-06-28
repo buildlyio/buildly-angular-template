f073c4275a9dbfc3cf7016d1096053d275512ec3
Aziz <aziz@humanitec.com>
2019-06-28 14:06:39 +0200

    set up styles seprately for midgard and admin

--- src/midgard-admin/midgard-admin.component.ts
+++ src/midgard-admin/midgard-admin.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'midgard-admin',
   templateUrl: './midgard-admin.component.html',
-  styleUrls: ['./midgard-admin.component.css']
+  styleUrls: ['./midgard-admin.component.scss']
 })
 export class MidgardAdminComponent implements OnInit {
 
